dc93b668ac4755c75fe6d0f89eb1b5ac8a4e5070
Administrator <Administrator@brianCheng>
2017-11-29 19:16:30 +0800

    外网地址

--- src/main/java/com/shopping/web/GoShoppingWebApplication.java
+++ src/main/java/com/shopping/web/GoShoppingWebApplication.java
@@ -9,6 +9,7 @@ public class GoShoppingWebApplication {
 	 * 1.启动方式（第一种右键GoShoppingWebApplication.java文件run运行启动，端口号是application.
 	 * properties的server.port 第二种，将项目用tomcat部署
 	 * 2.可以用JdbcTemplate访问数据库操作，也可用mybatis的xml访问编写
+	 * http://briancheng.51vip.biz/goShoppingWeb/
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(GoShoppingWebApplication.class, args);
